0d7789e85690287fbdfc0e15bb8c614febfd0337
Eman Nasir <eimannasir82@gmail.com>
2025-07-14 16:23:27 +0500

    Add files via upload

---
+++ PasswordStrenghtChecker/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter your password: ");
+        string password = Console.ReadLine();
+
+        int score = CalculateStrength(password);
+        Console.WriteLine("Password Strength: " + GetStrengthLevel(score));
+    }
+
+    static int CalculateStrength(string password)
+    {
+        int score = 0;
+
+        if (password.Length >= 8)
+            score++;
+
+        if (Regex.IsMatch(password, "[A-Z]"))
+            score++;
+
+        if (Regex.IsMatch(password, "[0-9]"))
+            score++;
+
+        if (Regex.IsMatch(password, "[^a-zA-Z0-9]"))
+            score++;
+
+        return score;
+    }
+
+    static string GetStrengthLevel(int score)
+    {
+        return score switch
+        {
+            4 => "Strong",
+            3 => "Medium",
+            _ => "Weak",
+        };
+    }
+}
